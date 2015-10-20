565caf6eaaa8987f6b73909d36c4688b5d86777b
Lotta Skogström <lotta.skogstrom@gmail.com>
2015-10-20 04:47:11 -0700

    toinen tunti

--- src/client.js
+++ src/client.js
@@ -1,3 +1,74 @@
-require('./client.css');
+import '../node_modules/normalize.css/normalize.css';
+import'./client.css';
 
-console.log('kvaak sanoo ankka!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Hello {this.props.name}
+            </div>
+        );
+    }
+});
+
+
+const HelloWorldApp = React.createClass({
+    render: function() {
+        const names = this.props.names;
+        return (
+            <div className="content">
+                <h1>Title</h1>
+                {names.map(name =>
+                    <HelloWorld name={name} />
+                )}
+                <Counterizer onIncrementCounter={this.incrementCounter} count={this.state.count} />
+                <Counter count={this.state.count} />
+            </div>
+        );
+    },
+    getInitialState: function() {
+        return {
+            count: 0
+        }
+    },
+    incrementCounter: function() {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+});
+
+const Counterizer = React.createClass({
+    render: function() {
+        return (
+            <div className="counter">
+                <div className="counter-number">
+                    {this.props.count}
+                </div>
+                <button onClick={this.props.onIncrementCounter}>More</button>
+            </div>
+        );
+    }
+});
+
+const Counter = React.createClass({
+    render: function() {
+        return (
+            <div className="mega-counter">
+                <p>Clicked: </p>
+                <div className="mega-counter-number">
+                    {this.props.count}
+                </div>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <HelloWorldApp names={['moi', 'Blaa', 'jaa']} />,
+    document.getElementById('app')
+);
+
